269d1766efdfa221c5e278e7b95f0cafd19bb916
ChangZhuo Chen <czchen@gmail.com>
2013-05-29 20:36:27 +0800

    Update test.py

--- test.py
+++ test.py
@@ -10,9 +10,17 @@ from fq_tree import FQTree
 def main():
     data = map(lambda x: x["bopomofo"], lib.load_tsi_src())
 
+    print("BKTree")
     bktree = BKTree()
     bktree.insert(data)
     bktree.print_statistic()
+    print("")
+
+    print("FQTree")
+    fqtree = FQTree()
+    fqtree.insert(data)
+    fqtree.print_statistic()
+    print("")
 
 
 if __name__ == "__main__":
