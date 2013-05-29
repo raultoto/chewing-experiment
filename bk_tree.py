289f8799a1f72af2f96d519aa9819b50c83bf68b
ChangZhuo Chen <czchen@gmail.com>
2013-05-29 19:14:43 +0800

    Record tree information

--- bk_tree.py
+++ bk_tree.py
@@ -9,11 +9,19 @@ class BKTree():
         self._distance_func = distance_func
         self._root = {}
 
+        self._counter = {}
+        for i in range(1, 12):
+            self._counter[i] = {
+                "insert": 0,
+                "node": 0,
+            }
+        self._latest_query_lookup = 0
+
     @staticmethod
     def _get_bopomofo_length(bopomofo):
         return len(bopomofo.split(" "))
 
-    def _insert(self, current, bopomofo):
+    def _insert(self, current, bopomofo, length):
         """This function inserts new bopomofo string to BK-tree
         """
 
@@ -25,11 +33,12 @@ class BKTree():
             current["children"] = {}
 
         if distance in current["children"]:
-            self._insert(current["children"][distance], bopomofo)
+            self._insert(current["children"][distance], bopomofo, length)
         else:
             current["children"][distance] = {
                 "bopomofo": bopomofo,
             }
+            self._counter[length]["node"] += 1
 
     def insert(self, bopomofo_list):
         """Insert bopomofo dictionary to BK-tree
@@ -37,18 +46,23 @@ class BKTree():
 
         for bopomofo in bopomofo_list:
             length = self._get_bopomofo_length(bopomofo)
+            if length == 12:
+                print(bopomofo)
+            self._counter[length]["insert"] += 1
 
             if length in self._root:
-                self._insert(self._root[length], bopomofo)
+                self._insert(self._root[length], bopomofo, length)
             else:
                 self._root[length] = {
                     "bopomofo": bopomofo,
                 }
+                self._counter[length]["node"] += 1
 
-    def _query(self, current, bopomofo, threshold):
+    def _query(self, current, bopomofo, length, threshold):
         result = []
 
         distance = self._distance_func(current["bopomofo"], bopomofo)
+        self._latest_query_lookup += 1
         if distance < threshold:
             result.append({
                 "bopomofo": current["bopomofo"],
@@ -58,7 +72,7 @@ class BKTree():
         for i in range(distance - threshold, distance + threshold + 1):
             if "children" in current and i in current["children"]:
                 result.extend(
-                    self._query(current["children"][i], bopomofo, threshold))
+                    self._query(current["children"][i], bopomofo, length, threshold))
 
         return result
 
@@ -78,11 +92,27 @@ class BKTree():
         """
 
         length = self._get_bopomofo_length(bopomofo)
+        self._latest_query_lookup = 0
         if length in self._root:
-            return self._query(self._root[length], bopomofo, threshold)
+            return self._query(self._root[length], bopomofo, length, threshold)
         else:
             return []
 
+    def print_statistic(self):
+        insert = 0
+        node = 0
+        for i in range(1, 12):
+            print("For length {}:".format(i))
+            print("\tInsert count is {}".format(self._counter[i]["insert"]))
+            print("\tTree node count is {}".format(self._counter[i]["node"]))
+            insert += self._counter[i]["insert"]
+            node += self._counter[i]["node"]
+        print("Total insert count is {}".format(insert))
+        print("Total tree node count is {}".format(node))
+
+
+    def print_latest_query_lookup():
+        print("Latest query lookup is {}".format(self._latest_query_lookup))
 
 if __name__ == "__main__":
     import doctest
